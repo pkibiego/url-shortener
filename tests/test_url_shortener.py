5abaf32032e2a3144f9e32804e45186183132c2f
Tito Kibiego <patrick.kibiego@thepalladiumgroup.com>
2024-10-28 15:09:39 +0300

    Minor changes to tests

--- tests/test_url_shortener.py
+++ tests/test_url_shortener.py
@@ -1,6 +1,12 @@
+import sys
+import os
 import pytest
 from fastapi.testclient import TestClient
-from ..main import app
+
+# Add the parent directory to the sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from main import app  # Absolute import
 
 client = TestClient(app)
 
